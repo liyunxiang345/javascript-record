489492fb854d4a921d3c437951910f0dd2487f46
李云翔 <liyunxiang345@163.com>
2020-08-07 11:43:15 +0800

    call和apply的实现

--- records/apply.js
+++ records/apply.js
@@ -1,3 +1,6 @@
+/**
+ * 模拟apply的实现
+ */ 
 Function.prototype._apply = function(_argument){
   if(typeof this !== "function"){
     throw TypeError("Error 缺少函数");
@@ -5,12 +8,14 @@ Function.prototype._apply = function(_argument){
   let result = null;
   // 判断是否存在第一个参数_arguments,没有则指向window
   _argument = _argument || window;
+  // 将函数所有者当作目标的属性
   _argument.fn = this;
   if(arguments[1]){
     result = _argument.fn(...arguments[1]);
   }else{
     result = _argument.fn()
   }
+  // 删除属性
   delete _argument.fn;
   return result;
 }
